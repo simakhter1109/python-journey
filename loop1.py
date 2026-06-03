802152871ab71cdf0c41ab57e36bb8b153b92967
Sim Akhter <simakhter1109@gmail.com>
2026-06-03 13:50:32 +0530

    loop

--- loop1.py
+++ loop1.py
@@ -4,3 +4,11 @@ for i in range(12):
     print("5 X", i +1, "=", 5 * (i+1))
 
 print("Loop eneded")
+
+
+for i in range(15):
+    if(i == 10):
+        print("Skip the iteratioin")
+        continue
+    print("5 X", i, "=", 5 * (i+1))
+          
