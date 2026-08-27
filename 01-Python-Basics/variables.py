d215a42355a851d459067332ea573e5127d29578
Sim Akhter <simakhter1109@gmail.com>
2026-08-28 00:24:48 +0530

    Update Python Basics

---
+++ 01-Python-Basics/variables.py
@@ -0,0 +1,80 @@
+# # STORE BASIC INFORMATION IN VARIABLES
+# name = "Sim"
+# age = 20
+# country = "India"
+# print(name)
+# print(age)
+# print(country)
+
+
+
+# # DIFFERENT TYPES OF VARIABLES
+# name = "Sim"
+# age = 20
+# height = 5.4
+# is_student = True
+
+# print(name)
+# print(age)
+# print(height)
+# print(is_student)
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
+
+
+
+# # CHANGING THE VALUE OF A VARIABLE
+# age = 20
+# print(age)
+
+# age = 21
+# print(age)
+
+# age = 22
+# print(age)
+
+
+
+# # USING VARIABLES IN CALCULATIONS
+# price = 500
+# quantity = 1
+# total = price * quantity
+# print("price: ", price)
+# print("Quantity: ", quantity)
+# print("Total: ", total)
+
+
+
+# # MULTIPLE VARIABLES IN ONE LINE
+# name, age, city = "Sim", 20, "Delhi"
+# print(name, age, city)
+
+
+
+# # TAKING INPUT FROM THE USER
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Your name is: ", name)
+# print("Your age is: ", age)
+
+
+
+# # CALCULATE AGE NEXT YEAR
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# next_year_age = age + 1
+
+# print("Hello", name)
+# print("Your current age is", age)
+# print("Next year you will be", next_year_age)
+
+
+
+# USING VARIABLES INSIDE A STRING
+name = "Sim"
+age = 20
+course = "Python"
