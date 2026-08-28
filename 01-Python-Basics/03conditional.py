18a21688252e01545c94219f546e1b5d414ee001
Sim Akhter <simakhter1109@gmail.com>
2026-08-28 19:02:52 +0530

    Update Python journey

---
+++ 01-Python-Basics/03conditional.py
@@ -0,0 +1,56 @@
+# # BASIC IF-ELSE
+# number = int(input("Enter a number: "))
+# if(number % 2 == 0):
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+
+
+# # STUDENT EVALUATION SYSTEM
+# print("\033[1mSTUDENT EVALUATION SYSTEM\033[0m")
+# name = str(input("Enter your name: "))
+# marks = float(input("Enter your marks: "))
+# attendance = float(input("Enter the attendance percentage: "))
+# if marks < 0 or marks > 100:
+#     print("Invalid marks.")
+# elif attendance < 0 or attendance > 100:
+#     print("Invalid attendance percentage.")
+# else:
+#     if marks >= 90:
+#         grade = "A+"
+#     elif marks >= 80:
+#         grade = "A"
+#     elif marks >= 70:
+#         grade = "B"
+#     elif marks >= 60:
+#         grade = "C"
+#     elif marks >= 45:
+#         grade = "D"
+#     else:
+#         grade = "F"
+    
+#     print(f"\nStudent: {name}")
+#     print(f"Grade: {grade}")
+
+#     if marks >= 45 and attendance >=75:
+#         print("\033[1mSTATUS: PASSED.\033[0m")
+        
+#         if marks >= 90 and attendance >= 90:
+#             print("\033[1mELIGIBLE FOR FULL SCHOLARSHIP.\033[0m")
+#         elif marks >= 80 and attendance >= 85:
+#             print("\033[1mELIGIBLE FOR PARTIAL SCHOLARSHIP.\033[0m")
+#         else:
+#             print("\033[1mNOT ELIGIBLE FOR SCHOLARSHIP.\033[0m")
+    
+#     else:
+#         print("\033[1mSTATUS: FAILED.\033[0m")
+
+#         if attendance < 75:
+#             print("\033[1mREASON: ATTENDANCE BELOW 75%.\033[0m")
+        
+#         if marks < 45:
+#             print("\033[1mREASON: MARKS BELOW PASSING CRITERIA..\033[0m")
+
+
+
---
+++ 07-OOP_Basics/classes_objects.py
@@ -0,0 +1,35 @@
+# # BASIC CLASS & OBJECTS
+# # Created a class then create an object
+
+# class Student:
+#     pass
+# student1 = Student() #object
+# student2 = Student() #object
+
+# print(student1)
+# print(student2)
+
+# # GIVE OBJETCS DATA
+# # Attributes = a simple piece of information/data that belongs to an object.
+
+# class Student:
+#     pass
+# student1 = Student()
+# student1.name = "Sim Akhter"
+# student1.age = 20
+# print(student1.name)
+# print(student1.age)
+
+# MULTIPLE OBJECTS
+class Student:
+    pass
+student1 = Student()
+student2 = Student()
+student1.name = "Sim AKhter"
+student1.age = 20
+
+student2.name = "Aayushi Banerjee"
+student2.age = 19
+
+print(student1.name, student1.age)
+print(student2.name, student2.age)
