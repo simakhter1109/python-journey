b28d33f110ac0a4a805b5dcaffc8a1213ab49270
Sim Akhter <simakhter1109@gmail.com>
2026-09-14 21:15:56 +0530

    Updated OOP basics

--- 07-OOP_Basics/basics-1.py
+++ 07-OOP_Basics/basics-1.py
@@ -7,12 +7,25 @@
 # print(student1.age)
 
 
-# MULTIPLE OBJECTS
+# # MULTIPLE OBJECTS
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# student1 = Student("Sim", 20)
+# student2 = Student("Sahil", 22)
+# print(student1.name)
+# print(student2.age)
+
+
+
+# CONSTRUCTOR
 class Student:
-    def __init__(self, name, age):
+    def __init__(self, name, age, marks):
         self.name = name
         self.age = age
-student1 = Student("Sim", 20)
-student2 = Student("Sahil", 22)
-print(student1.name)
-print(student2.age)
+        self.marks = marks
+student = Student ("Sahil", 22, 96)
+print(student.name)
+print(student.age)
+print(student.marks)
